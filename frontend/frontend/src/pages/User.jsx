bd5c315bd4d42f501da5e3b20d6eb5ade0c402e6
Prabisha01 <prabishakhadka2000@gmail.com>
2026-06-04 09:58:49 +0545

    fetched succsfully

--- frontend/frontend/src/pages/User.jsx
+++ frontend/frontend/src/pages/User.jsx
@@ -17,7 +17,30 @@ const User = () => {
     fetchUser();
   }, []);
 
-  return <div>User</div>;
+  return (
+    <div>
+      <table>
+        <thead border="1">
+          <tr>
+            <th>Id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.image}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default User;
