76b606218da0f9cfa143f7929e46afdc81372cd3
Prabisha01 <prabishakhadka2000@gmail.com>
2026-06-16 09:52:19 +0545

    authmiddleware

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -9,9 +9,11 @@ const {
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/uploads");
+const { verifyToken } = require("../middleware/verifyToken");
+const { isAdmin } = require("../middleware/authMiddleware");
 router.post("/create", upload.single("image"), addUser);
 router.post("/login", login);
-router.get("/getAll", getAllUserFromTheDB);
+router.get("/getAll", verifyToken, isAdmin, getAllUserFromTheDB);
 router.get("/getById/:id", getUserByIDDB);
 router.delete("/deleteById/:id", deleteUserByIDDB);
 router.put("/updateById/:id", upload.single("image"), updateUserIDBD);
